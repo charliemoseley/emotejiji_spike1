852993c2f5dba62533101629bb2a17560af0072f
Charlie Moseley <charlie@robopengu.in>
2013-02-06 05:17:17 -0800

    Adds an all rule with counting ability to emoticons.

--- models/emoticon.rb
+++ models/emoticon.rb
@@ -6,4 +6,6 @@ class Emoticon
   property :description
   property :number_of_lines,    type: Fixnum, index: :fulltext
   property :longest_line_count, type: Fixnum
+  
+  rule :all, functions: [Neo4j::Wrapper::Rule::Functions::Size.new]
 end
