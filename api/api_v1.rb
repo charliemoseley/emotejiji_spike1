cb24f79c232ae8345af1d12e2b3b63d62ee092d2
Charlie Moseley <charlie@robopengu.in>
2013-02-08 03:17:12 -0800

    Adds a proper creation method.

--- api/api_v1.rb
+++ api/api_v1.rb
@@ -16,25 +16,12 @@ module Emotejiji
       end
     end
     
-    resource :emoticon do
+    resource :emoticons do
       desc "Returns emoticons."
       
-      get :create do
+      post '/' do
         Neo4j::Transaction.run do
-          Emoticon.new uid: "foobar#{rand(5)}", text: "^_^", description: "a smiley face", 
-                       number_of_lines: 1, characters_in_longest_line: 10
-        end
-        
-        { success: "yes" }
-      end
-      
-      get ':uid' do
-        e = Emoticon.find(uid: params[:uid]).first
-        
-        if e.nil?
-          { response: "sorry no results" }
-        else
-          { text: e.text }
+          Emoticon.new params
         end
       end
     end
